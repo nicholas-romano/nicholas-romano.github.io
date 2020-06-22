1cc5ef1f5b35dc27646782f22fdb6ce24e1aafc8
Nick Romano <njr7romano@yahoo.com>
2020-06-22 13:53:00 -0400

    Updated Portfolio, removed unused projects

--- Triranbulldogs/common/banner.php
+++
@@ -1,3 +0,0 @@
-<div class="banner">
-  <img src="images/triran-bulldogs-banner.png" class="img-responsive" alt="Puppies for sale. English Bulldogs" />
-</div>
--- Triranbulldogs/common/footer.php
+++
@@ -1,4 +0,0 @@
-
-<footer>
-	<p>Triran Bulldogs | (703) 282-6573 | 2014 | Spring, TX</p>
-</footer>
